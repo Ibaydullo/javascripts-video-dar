e62b99692e997d35fdf8973ee5b46f0df840b717
Ibayddullo <ubaydillosaydumaarov4@gmail.com>
2026-02-21 14:34:40 +0500

    video darslik 1 dan 30 gacha bajarilgan

--- javascript_video_dars_2/Lesson10/dars.js
+++ javascript_video_dars_2/Lesson10/dars.js
@@ -1,3 +1,44 @@
 /* ---------ko'p ishlatilinadigon string metodlari ---- */
 
 let email = 'ibaydullo39@gmail.com'
+
+
+// lostIndeOf
+// let results = email.lastIndexOf('a')
+// console.log(results)
+
+
+// // indexOf
+// let results2 = email.indexOf('a')
+// console.log(results2)
+
+
+// slice()
+// let results = email.slice(10)
+// console.log(results )
+
+// substr()
+// let results = email.substr(4, 10)
+// console.log(results)
+
+// // replace()
+// let results = email.replace('i', 'u')
+// console.log(results)
+
+
+// CharAt()
+// let results = email.charAt()
+// console.log(results)
+
+// roUpperCase() - toLowerCase()
+// let results = email.toUpperCase()
+// let results = email.toLowerCase()
+// console.log(results)
+
+// trim()
+// let results = email.trim()
+// console.log(results)
+
+// splice()
+// let results = email.split()
+// console.log(results)
