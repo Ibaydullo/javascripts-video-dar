6b9cbdc0ffba929d03162749dd25ee6270c1b566
Ibayddullo <ubaydillosaydumaarov4@gmail.com>
2026-02-23 13:16:16 +0500

    video darslik

---
+++ javascript_video_dars_2/Lesson11/dars.js
@@ -0,0 +1,78 @@
+let radius = 10
+let pi = 3.14
+
+
+// console.log(radius, pi)
+
+
+// math operators + ,-,*,/,**,%
+
+// console.log(10 / 2)
+
+
+// let result = 199 % 100
+
+// let result = pi * radius ** 2
+
+// ondr of operation
+
+// let result = 5 * (10 - 3) ** 2
+
+// let likes = 10
+
+// likes = likes + 1
+// likes += 12
+// likes -= 12
+
+
+
+// console.log(likes)
+
+
+
+// console.log(resut)
+
+
+
+// NN - not a number
+
+
+// console.log(5 / 'salom  ')
+
+
+// let result = 'ten blog has' + likes + 'likes'
+
+// console.log(result)
+
+
+
+
+
+// template string
+const title = 'Rastamojka qachon ochiladi ?! yohud Monopoliya Respublikasi !'
+const author = "Lag'mon makazi"
+const likes = 35000000
+
+
+
+
+// concatentation way
+// let result = 'Bu post nomi ' + title + ' ' + author + 'tomonidan yozilgan '+ 'va bu postda '+ 'ta likes bor'
+
+// console.log(result)
+
+
+
+
+// template string way
+
+// let result = `Bu post nomi ${title}, ${author} tomonidan yozilgan. Va bu postda ${likes} ta like bor`
+// console.log(result)
+
+let result = `
+    <h2>${title}</2>
+    <p>${author}</p>
+    <span>${title}</span>
+`
+
+console.log(result)
