07516324c16476f82067618830d35e1dcc9ba8ed
Ibaydullo <159132769+Ibaydullo@users.noreply.github.com>
2026-02-28 12:44:57 +0500

    58 darslik
    58 darslik

---
+++ javascript_video_dars_2/Lesson58/dars.js
@@ -0,0 +1,12 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+        <div class="key">${e.key == ' ' ? 'Space' : e.key}<small>event.key</small></div>
+        <div class="key">${e.code}<small>event.code</small></div>
+        <div class="key">${e.keyCode}<small>event.keyCode</small></div>
+    `
+    console.log('event.key', e.key)
+    console.log('event.keyCode', e.keyCode)
+    console.log('event.code', e.code)
+})
