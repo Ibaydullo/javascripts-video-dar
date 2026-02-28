eef38c0990399bf09a41217fd5269a2ff01fbf03
Ibaydullo <159132769+Ibaydullo@users.noreply.github.com>
2026-02-28 11:45:37 +0500

    dars 53 ichida 56 ham yozildi
    dars 53 ichida 56 ham yozildi

--- javascript_video_dars_2/Lesson53/dars.js
+++ javascript_video_dars_2/Lesson53/dars.js
@@ -1,12 +1,12 @@
 const signupForm = document.querySelector('#signup-form')
 // const username = document.querySelector('#username')
 const message = document.querySelector('.message')
+const regEX = /^[a-zA-Z0-9]{6,12}$/
 
 
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log(username.value)
-    const regEX = /^[a-zA-Z0-9]{6,12}$/
     const inputVal = signupForm.username.value
     if (regEX.test(inputVal)) {
         message.textContent = "Malumot to'g'ri kiritildi"
@@ -15,6 +15,19 @@ signupForm.addEventListener('submit', (e) => {
         Kiritilayotgan ma'lumot 6 va 12 ta malumot orasida bo'lishi kerak 
         kata va kichik lotin hariflaridan ibortab bo'lishii zarur. 
         Raqamlar ham aralashishi mumkin  `
+    }
+})
+
+
+
+
+// live feedback
+
+signupForm.username.addEventListener('keyup', (e) => {
+    if (regEX.test(e.target.value)) {
+        signupForm.username.setAttribute('class', 'success')
+    } else {
+        signupForm.username.setAttribute('class', 'error')
     }
 })
 
