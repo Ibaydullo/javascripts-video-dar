8a59fe4a0c2708adeb6db1423a855f046d82c264
Ibaydullo <159132769+Ibaydullo@users.noreply.github.com>
2026-03-09 13:08:55 +0500

    79lesson
    79lesson

--- javascript_video_dars_2/Lesson78/dars.js
+++ javascript_video_dars_2/Lesson78/dars.js
@@ -1,29 +1,59 @@
 const getTodos = (resurse, callback) => {
-    const request = new XMLHttpRequest()
-    request.addEventListener('readystatechange', () => {
-        // console.log(request,request.readyState)
-        if (request.readyState === 4 && request.status === 200) {
-            const date = JSON.parse(request.responseText)
-            callback(undefined, date)
-        } else if (request.readyState === 4) {
-            callback("Malumotni iloji bolmadi !!!", undefined)
-        }
-    })
-    // OPEN
-    request.open('GET', resurse)
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
+        request.addEventListener('readystatechange', () => {
+            // console.log(request,request.readyState)
+            if (request.readyState === 4 && request.status === 200) {
+                const date = JSON.parse(request.responseText)
+                resolve(date)
+            } else if (request.readyState === 4) {
+                reject("Malumotni iloji bolmadi !!!",)
+            }
+        // OPEN
+        request.open('GET', resurse)
 
-    // send
-    request.send()
+        // send
+        request.send()
+    })
 }
 
-getTodos('todos/todos.json', (err, data) => {
+
+getTodos('totos/todos.json').then((data) =>{
     console.log(data)
-    getTodos('todos/todos.json', (err, data) => {
-        console.log(data)
-        getTodos('todos/todos.json', (err, data) => {
-            console.log(data)
-        })
-    })
 })
 
 
+
+// getTodos('todos/todos.json', (err, data) => {
+//     console.log(data)
+//     getTodos('todos/todos.json', (err, data) => {
+//         console.log(data)
+//         getTodos('todos/todos.json', (err, data) => {
+//             console.log(data)
+//         })
+//     })
+// })
+
+
+
+// promise
+
+const internet = false
+
+const getData = () => {
+    return new Promise((resolve, reject) => {
+        // serverga sorov jonatish
+        if (internet) {
+            resolve("salom date")
+        } else {
+            reject("hayr error")
+        }
+    })
+}
+
+
+getData().then((a) => {
+    console.log(a)
+}).catch((b) => {
+    console.log(b)
+})
